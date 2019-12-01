b230a6fa1e53afdaa4dfdfd9ec069cce50a3611d
Steve Song <sshtel@gmail.com>
2019-12-01 15:59:58 +0900

    added monsters-rolodex app

---
+++ monsters-rolodex/src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      string: 'Hello Steve'
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {this.state.string}
+          <button>Click</button>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
