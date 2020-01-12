144f0702f4ffb00291d1dcc2013cdcf65c7dac16
Steve Song <sshtel@gmmail.com>
2020-01-13 00:03:33 +0900

    Updates

---
+++ precache-manifest.506798589248c66dd2352bf9d33ead52.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbb6af806b484077d55904c297ebcc02",
+    "url": "/react-practice/index.html"
+  },
+  {
+    "revision": "cbd94964dbc5c3b2d25c",
+    "url": "/react-practice/static/css/main.f83a13ce.chunk.css"
+  },
+  {
+    "revision": "0c65a12eaa1097e0c6b7",
+    "url": "/react-practice/static/js/2.1eede1e0.chunk.js"
+  },
+  {
+    "revision": "cbd94964dbc5c3b2d25c",
+    "url": "/react-practice/static/js/main.cffad5e6.chunk.js"
+  },
+  {
+    "revision": "84248220947f773f964e",
+    "url": "/react-practice/static/js/runtime-main.9517e3d1.js"
+  }
+]);
